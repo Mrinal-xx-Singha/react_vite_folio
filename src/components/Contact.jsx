aa23c86d875ea8de976927117de0c829a8aa203d
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2023-09-27 13:38:47 +0530

    added the scroll effect

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name="contact" className='w-full h-screen bg-[#080808] flex justify-center items-center p-4'>
-      <form action="" className='flex flex-col max-w-[600px] w-full'>
+      <form method='POST' action="https://getform.io/f/409c30b6-a341-48cd-9d2e-7296b6e46daa" className='flex flex-col max-w-[600px] w-full'>
         <div className='pb-8 '>
             <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'> Contact</p>
 
